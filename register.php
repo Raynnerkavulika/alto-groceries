098e6bcb6503020f3b4e97eade27ce97b7f0b6c4
altoid <kavulikar@gmail.com>
2024-12-14 13:52:16 +0300

    updated register and css code

--- register.php
+++ register.php
@@ -120,6 +120,12 @@ function sendVerificationSMS($phone, $token) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- custom css file link -->
+     <link rel="stylesheet" href="style.css">
+     <!-- font awesome cdn link -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+
     <title>register page</title>
 </head>
 <body>

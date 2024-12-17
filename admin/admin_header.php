b9ff9e15fbf2e35c83b2c2c520dd92580ccf46e1
altoid <kavulikar@gmail.com>
2024-12-17 18:04:30 +0300

    updated

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -6,5 +6,10 @@
             <a href="admin_dashboard.php">home</a>
             <a href="admin_products.php">products</a>
         </nav>
+
+        <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <div id="user-btn" class="fas fa-user"></div>
+        </div>
     </div>
 </header>

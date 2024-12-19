3dc7ddc8c7b482bc69c147e6c192e89b1aea1cf6
altoid <kavulikar@gmail.com>
2024-12-19 17:09:42 +0300

    updated

--- header.php
+++ header.php
@@ -1,3 +1,28 @@
 <header class="header">
+  <div class="nav nav-1">
+   <section class="flex">
+    <a href="index.php" class="logo">alto groceries</a>
 
+    <div class="search-box">
+        <form action="" method="post">
+           <input type="search" class="box" name="search_box" placeholder="type to search products..">
+           <input type="submit" class="btn" value="search" name="search_btn">
+        </form>
+    </div>
+    
+    <div>
+        <p><i class="fas fa-user"></i>Hello, <a href="login.php">login</a> | <a href="register.php">register</a></p>
+    </div>
+
+    <a href="cart.php"><i class="fas fa-shopping-cart"></i>cart</a>
+   </section>
+  </div>
+
+  <div class="nav nav-2">
+      <section class="flex">
+         <ul>
+            <li></li>
+         </ul>
+      </section>
+  </div>
 </header>

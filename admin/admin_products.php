c3bbb72d71d402bb8d16ff72253dd1c598c14a93
altoid <kavulikar@gmail.com>
2025-01-13 17:39:10 +0300

    updated the project

--- admin/admin_products.php
+++ admin/admin_products.php
@@ -4,8 +4,11 @@ include '../config.php';
 session_start();
 
 if(isset($_POST['add_product'])){
-    $name = $_POST['namme'];
+    $name = $_POST['name'];
     $name = filter_var($name,FILTER_SANITIZE_STRING);
+    $price = $_POST['price'];
+    $price = filter_var($price,FILTER_SANITIZE_STRING);
+    
 }
 
 ?>
